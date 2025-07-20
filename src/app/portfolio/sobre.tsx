ef7b038802a40a43103eff0390ba5d64e835633d
Marcos-GC-Ramos <marcosgustavoramos72@gmail.com>
2025-07-19 23:02:02 -0400

    adicionando novas informacoes

--- src/app/portfolio/sobre.tsx
+++ src/app/portfolio/sobre.tsx
@@ -1,15 +1,14 @@
 export default function Sobre() {
   return ( 
-    <section id="sobre" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About me">
+    <section id="sobre" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Sobre mim">
         <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-            <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
+            <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Sobre</h2>
         </div>
         
         <div>
-            <p className="mb-4">I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.</p>
-            <p className="mb-4">Currently, Im a Senior Front-End Engineer at , specializing in accessibility. I contribute to the creation and maintenance of UI components that power Klaviyo’s frontend, ensuring our platform meets web accessibility standards and best practices to deliver an inclusive user experience.</p>
-            <p className="mb-4">In the past, Ive had the opportunity to develop software across a variety of settings — from additionally, I also released a few years ago, guiding learners through building a web app with the Spotify API.</p>
-            <p>In my spare time, I’m usually climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for .</p>
+            <p className="mb-4">Sou um desenvolvedor apaixonado por criar interfaces de usuário acessíveis, onde design inteligente encontra engenharia sólida. Tenho um olhar atento para detalhes e me especializo em transformar ideias em experiências digitais elegantes, intuitivas e de alto desempenho. Meu trabalho ganha vida na interseção entre design e desenvolvimento, onde estética, funcionalidade e usabilidade caminham lado a lado.</p>
+            <p className="mb-4">Atualmente, atuo como desenvolvedor front-end na Secretaria de Educação do Amazonas (Seduc/AM), onde contribuo para a criação e manutenção de componentes de interface que impulsionam os sistemas da secretaria. Meu foco está em desenvolver soluções intuitivas, acessíveis e sustentáveis, que otimizam processos internos e melhoram a experiência dos usuários em plataformas voltadas à gestão educacional.</p>
+            <p>No meu tempo livre, estou geralmente em busca de novas inspirações para minhas criações, estudando e revisando tecnologias de front-end e simplesmente me divertindo jogando com os meus amigos.</p>
         </div>
     </section>
   );
