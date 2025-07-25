94ac3810e6f68303379b3578b3c8461333b978e5
Marcos-GC-Ramos <marcosgustavoramos72@gmail.com>
2025-07-25 09:59:48 -0400

    alteracao de margin no titulo de skills

--- src/app/portfolio/skills.tsx
+++ src/app/portfolio/skills.tsx
@@ -4,7 +4,7 @@ export default function Skills() {
     return (
         <section id="skills" className="mb-16 scroll-mt-16 md:mb-28 lg:scroll-mt-24" aria-label="Skills de desenvolvedor web">
             <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
-                <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 mb-8">
+                <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:mb-8">
                     Skills
                 </h2>
             </div>
