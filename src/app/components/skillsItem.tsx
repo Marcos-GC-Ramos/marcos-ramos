ed3e0f0e6aa30d18987c8b724155920711cbc72b
Marcos-GC-Ramos <marcosgustavoramos72@gmail.com>
2025-07-24 20:16:33 -0400

    reajustando o layout

--- src/app/components/skillsItem.tsx
+++ src/app/components/skillsItem.tsx
@@ -5,16 +5,22 @@ interface SkillsItemProps {
   titulo: string;
 }
 
-const ExperienceItem: React.FC<SkillsItemProps> = ({
+const SkillItem: React.FC<SkillsItemProps> = ({
+  titulo,
   technologies,
-  titulo
 }) => {
   return (
     <li className="mb-12">
-      <h1>{titulo}</h1>
-      <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4">
-        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block"></div>
+      <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
         <div className="z-10 sm:col-span-6">
+          <header
+            className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+            aria-label={titulo}
+          >
+            {titulo}
+          </header>
+
           <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
             {technologies.map((tech, index) => (
               <li key={index} className="mr-1.5 mt-2">
@@ -30,4 +36,5 @@ const ExperienceItem: React.FC<SkillsItemProps> = ({
   );
 };
 
-export default ExperienceItem;
+export default SkillItem;
+
