06c6c7665c990a61c41e64b27a03b63fdc874f12
Marcos-GC-Ramos <marcosgustavoramos72@gmail.com>
2025-08-05 14:40:29 -0400

    uma nova animação

---
+++ src/app/components/animacaoMR.tsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function AnimacaoMR() {
+  return (
+        <Spline
+            scene="https://prod.spline.design/kTT8XZNld317TbJ2/scene.splinecode"
+            className="fixed top-0 left-0 z-[-1] w-[200px] h-[200px]"
+        />
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-// import LightEffect from '../components/lightEffect'; ainda nao esta pronto
 import InfoFixas from './portfolio/info-fixas';
 import Sobre from './portfolio/sobre';
 import Experiencia from './portfolio/experiencia';
 import Skills from './portfolio/skills';
-// import Projetos from './portfolio/projetos'; ainda nao esta pronto
 import Footer from './portfolio/footer';
+
+// import LightEffect from '../components/lightEffect'; ainda nao esta pronto
+// import Projetos from './portfolio/projetos'; ainda nao esta pronto
+// import AnimacaoMR from './components/animacaoMR'; ainda nao esta pronto
 
 export default function Home() {
   return (
     <> 
-      {/* <LightEffect/> */}
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <InfoFixas/>
@@ -18,7 +19,6 @@ export default function Home() {
             <Sobre/>
             <Skills />
             <Experiencia/>
-            {/* <Projetos /> */}
             <Footer />
           </main>
         </div>
